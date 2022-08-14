d5d36382fb378fac154f7373836f72e5a108c3f7
Administrator+1-at-592373886397 <ahsaeldin@gmail.com>
2022-08-14 21:06:05 +0200

    Update models.py
    Added Keyword and Quote models.

--- dj_imdb_quotes/movies_quotes/quotes_app/models.py
+++ dj_imdb_quotes/movies_quotes/quotes_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Keyword(models.Model):
+    keyword_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Quote(models.Model):
+    question = models.ForeignKey(Keyword, on_delete=models.CASCADE)
+    quote_text = models.CharField(max_length=2000)
+    votes = models.IntegerField(default=0)
