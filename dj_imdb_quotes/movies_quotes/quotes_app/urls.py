185b1acd977ce24288be9fb3841c8fc7fdbc322a
Administrator+1-at-592373886397 <ahsaeldin@gmail.com>
2022-08-15 23:25:40 +0200

    First Template, First View
    Added the first Index view and the index template, and updated the urls.py with the paths
    
    Added 3 other stub views for now.

--- dj_imdb_quotes/movies_quotes/quotes_app/urls.py
+++ dj_imdb_quotes/movies_quotes/quotes_app/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /quotes_app/5/
+    path('<int:keyword_id>/', views.detail, name='detail'),
+    # ex: /quotes_app/5/results/
+    path('<int:keyword_id>/results/', views.results, name='results'),
+    # ex: /quotes_app/5/vote/
+    path('<int:keyword_id>/vote/', views.vote, name='vote'),
 ]
--- dj_imdb_quotes/movies_quotes/quotes_app/views.py
+++ dj_imdb_quotes/movies_quotes/quotes_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Keyword
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the quotes index.")
+    latest_keyword_list = Keyword.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('quotes_app/index.html')
+    context = {
+        'latest_keyword_list': latest_keyword_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, keyword_id):
+    return HttpResponse("You're looking at keyword %s." % keyword_id)
+
+
+def results(request, keyword_id):
+    response = "You're looking at the results of keyword %s."
+    return HttpResponse(response % keyword_id)
+
+
+def vote(request, keyword_id):
+    return HttpResponse("You're voting on keyword %s." % keyword_id)
