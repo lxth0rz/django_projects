8ab4f680076b1ff72db48fe3cdd82560c3c24905
Administrator+1-at-592373886397 <ahsaeldin@gmail.com>
2022-08-18 22:03:16 +0200

    templates and views added.
    Added/Modified
    
    templates:
         results.html
         detail.html
    
    Views:
          results
          vote

--- dj_imdb_quotes/movies_quotes/quotes_app/views.py
+++ dj_imdb_quotes/movies_quotes/quotes_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
-from .models import Keyword
+from .models import Keyword, Quote
+from django.urls import reverse
 
 
 def index(request):
@@ -19,9 +20,26 @@ def detail(request, keyword_id):
 
 
 def results(request, keyword_id):
-    response = "You're looking at the results of keyword %s."
-    return HttpResponse(response % keyword_id)
+    keyword = get_object_or_404(Keyword, pk=keyword_id)
+    return render(request, 'quotes_app/results.html', {'keyword': keyword})
 
 
 def vote(request, keyword_id):
-    return HttpResponse("You're voting on keyword %s." % keyword_id)
+    keyword = get_object_or_404(Keyword, pk=keyword_id)
+
+    try:
+        selected_quote = keyword.quote_set.get(pk=request.POST['quote'])
+    except (KeyError, Quote.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'quotes_app/detail.html', {
+            'keyword': keyword,
+            'error_message': "You didn't select a quote.",
+        })
+    else:
+        selected_quote.votes += 1
+        selected_quote.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('quotes_app:results', args=(keyword.id,)))
+
